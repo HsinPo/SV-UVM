44b275fb1e88eca105639bbc1ae0722dc55a416d
Hsin-Po Lin <stillmark@hotmail.com>
2026-01-16 15:04:25 +0800

    optimize process

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1. clean
+if [ -d "work" ]; then
+    rm -rf work
+fi
+
+# 2. establish
+vlib work
+
+# 3. compile
+vlog *.sv
+
+# 4. open GUI and load signal/wave
+# -do "..." 
+# add wave -r /*
+# run -all
+vsim -voptargs=+acc -do "add wave -r /*; run -all; wave zoom full" top
--- testbench.sv
+++ testbench.sv
@@ -1,5 +1,20 @@
 module top;
+  logic clk;
+  logic [7:0] counter;
+
+
   initial begin
-    $display("Hello UVM!");
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    counter = 0;
+    repeat (10) @(posedge clk) begin
+      counter = counter + 1;
+    end
+    
+    $display("Test Finished!");
+    $finish;
   end
 endmodule
